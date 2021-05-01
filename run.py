a2b1b30ed8b4b11fe2ecb49fcddc48b414048aea
BXS-Archipelago <bxs@tutanota.com>
2021-05-01 09:46:12 +0000

    add calculated surplus data to surplus worksheet

--- run.py
+++ run.py
@@ -40,7 +40,7 @@ def get_sales_data():
 def validate_data(values):
     """
     Inside the try, converts all string values into integers.
-    Raises ValueError if strings cannit be converted into int, or if there aren't exactly 6 values.
+    Raises ValueError if strings cannit be converted into int, or if there are not exactly 6 values.
     """
     try: 
         [int(value) for value in values]
@@ -62,6 +62,15 @@ def update_sales_worksheet(data):
     sales_worksheet = SHEET.worksheet("sales")
     sales_worksheet.append_row(data)
     print("Sales worksheet updated successfully. \n")
+
+def update_surplus_worksheet(data):
+    """
+    Updadate sales worksheet, add new row with the list data provided
+    """
+    print("updating sales worksheet...\n")
+    surplus_worksheet = SHEET.worksheet("surplus")
+    surplus_worksheet.append_row(data)
+    print("Surplus worksheet updated successfully. \n")
 
 
 def calculate_surplus_data(sales_row):
@@ -93,7 +102,7 @@ def main():
     sales_data = [int(num) for num in data]
     update_sales_worksheet(sales_data)
     new_surplus_data = calculate_surplus_data(sales_data)
-    print(new_surplus_data)
+    update_surplus_worksheet(new_surplus_data)
 
 print("Welcome to the Sandwich Data Automation!!!")
 

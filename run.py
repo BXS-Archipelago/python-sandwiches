3ee68a776a161c9f4ff07e36b81e4db536c195de
BXS-Archipelago <bxs@tutanota.com>
2021-05-01 10:57:18 +0000

    update with average plus 10percent

--- run.py
+++ run.py
@@ -110,7 +110,7 @@ def get_last_5_entries_sales():
 
     """
     sales = SHEET.worksheet("sales")
-    )
+    
 
     columns = []
     for ind in range(1,7):
@@ -118,6 +118,22 @@ def get_last_5_entries_sales():
         columns.append(column[-5:])
         
     return columns
+
+def calculate_stock_data(data):
+    """
+    Run all program functions
+    """
+
+    print("calculating stock data... \n")
+    new_stock_data = []
+
+    for column in data: 
+        int_column = [int(num) for num in column]
+        average = sum(int_column)/len(int_column)
+        stock_num = average * 1.1
+        new_stock_data.append(round(stock_num))
+
+    return new_stock_data
 
 
 def main():
@@ -129,10 +145,10 @@ def main():
     sales_data = [int(num) for num in data]
     update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_data(sales_data)
-    update_worksheet(new_surplus_data, "surplus")
+    update_worksheet(new_surplus_data, "surplus")    
+    sales_columns = get_last_5_entries_sales()
+    stock_data = calculate_stock_data(sales_columns)
 
 print("Welcome to the Sandwich Data Automation!!!")
 
 main()
-
-sales_columns = get_last_5_entries_sales()
